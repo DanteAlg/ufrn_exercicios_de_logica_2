b5a2241e78a616e08020b35a7296ee870d8a8d00
Dante <dante.alighierimds@gmail.com>
2015-08-11 19:36:59 -0300

    Exercício 8

---
+++ exercicio_8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  int numero;
+
+  cout << "Escreva um número: ";
+  cin >> numero;
+  cout << endl;
+
+  if ((numero % 4 == 0) && (numero % 5 == 0) && (numero % 3 == 0)) {
+    cout << "Seu número é divisível por 4, 5 e 3!";
+  }
+  else {
+    cout << "Não divisível";
+  }
+
+  cout << endl;
+}
